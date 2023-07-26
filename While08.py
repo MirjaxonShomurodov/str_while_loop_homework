b13d9e3bf66297d5e5b2ec2d8ec2f8460dbf6615
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-26 12:41:27 +0500

    While08.py

--- While08.py
+++ While08.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=len(s)
+    s=int(s)
+    i=0
+    toq=0
+    while i<a:
+        if (s%10)%2==1:
+            toq+=1
+        i+=1
+        s//=10
+    return toq
+print(main("1234567890"))
